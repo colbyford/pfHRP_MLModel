eedb1c2095d0755822239350e89a805ecbf74186
Colby Ford <colbytylerford@gmail.com>
2020-02-26 10:48:39 -0500

    Add data pivot for ML modeling

--- ML_Modeling.R
+++ ML_Modeling.R
@@ -8,12 +8,18 @@ library(readr)
 library(tidyr)
 
 ## Load in Motif Data
-fastas_df <- read_csv("pfHRP_Motif_Matches.csv") %>% filter(gene == "pfhrp2")
+fastas_df <- read_csv("pfHRP_Motif_Matches.csv")
+hrp2s <- fastas_df %>% filter(gene == "pfhrp2")
+hrp3s <- fastas_df %>% filter(gene == "pfhrp3")
+all_hrps <- hrp2s %>% full_join(hrp3s, by = "id", suffix = c("_pfhrp2", "_pfhrp3"))
+
 ## Load in metadata
 metadata <- read_csv("metadata.csv")
 
 ## Join Together
-full_df <- fastas_df %>%
+# full_df <- fastas_df %>%
+full_df <- all_hrps %>%
+  na.omit() %>% ## Comment out to include missing values
   inner_join(metadata, by = c("id" = "Id")) %>% 
   mutate_at(c("workLiving",
               "Address",
@@ -24,7 +30,10 @@ full_df <- fastas_df %>%
               "qPCRcateg34",
               "paradencateg"), as.factor)
 
-model_formula <- as.formula(paste("PfHRP2 ~ ", paste(paste("Type_", 1:24, sep=""), collapse= "+")))
+model_formula <- as.formula(paste("PfHRP2 ~ ",
+                                  paste(paste("Type_", 1:24, "_pfhrp2", sep=""), collapse= "+"),
+                                  "+",
+                                  paste(paste("Type_", 1:24, "_pfhrp3", sep=""), collapse= "+")))
 
 lr_mod <- glm(model_formula,
               data = full_df,
