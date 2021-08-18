0f8c504f40193a1de47b5031fa2abd5238861a88
Colby Ford <colbytylerford@gmail.com>
2021-08-18 16:09:32 -0400

    Add chi^2 statistical comparison

--- chi_sq_test.R
+++ chi_sq_test.R
@@ -1,7 +1,28 @@
+## Chi Squared Tests for RDT results comparison
 library(readr)
 library(dplyr)
 
-data <- read_csv("pfHRP2_withMeta.csv") %>%
-  select(id, PfHRP2, qPCRcateg34)
+data <- read_csv("results_comparison.csv")
 
 chisq.test(data$qPCRcateg34, data$PfHRP2, correct = FALSE)
+chisq.test(data$PfHRP2, data$qPCRcateg34,  correct = FALSE)
+chisq.test(data$qPCRcateg34, data$qPCRcateg34, correct = FALSE)
+chisq.test(data$PfHRP2, data$PfHRP2, correct = FALSE)
+
+## Types 2 and 7
+chisq.test(data$qPCRcateg34, data$ML_VE_2_7, correct = FALSE)
+chisq.test(data$qPCRcateg34, data$ML_ERT_2_7, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_VE_2_7, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_ERT_2_7, correct = FALSE)
+
+## Types 1 thru 24
+chisq.test(data$qPCRcateg34, data$ML_VE_1thru24, correct = FALSE)
+chisq.test(data$qPCRcateg34, data$ML_ERT_1thru24, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_VE_1thru24, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_ERT_1thru24, correct = FALSE)
+
+## Type 3 and 5 and 10
+chisq.test(data$qPCRcateg34, data$ML_VE_3_5_10, correct = FALSE)
+chisq.test(data$qPCRcateg34, data$ML_ERT_3_5_10, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_VE_3_5_10, correct = FALSE)
+chisq.test(data$PfHRP2, data$ML_ERT_3_5_10, correct = FALSE)
